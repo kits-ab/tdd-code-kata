0a091e0bb13785ec31d94f2e20e9483660a5a7c9
Tobias Lans <tobias.lans@gmail.com>
2021-03-26 14:27:50 +0100

    Added another exercise

---
+++ katas/shopping/shopping.ts
@@ -0,0 +1,13 @@
+/**
+ * Shopping list
+ *
+ * Find the items which I always purchase
+ *
+ * Given several lists of shopping items find the always re-occuring items, e.g.
+ * [
+ *   ['bread', 'milk', 'butter'],
+ *   ['honey', 'flour', 'milk' ]
+ * ]
+ * should produce a result object: { itemCount: 1, items: ['milk'] }
+ */
+export const commonItems = (_: any) => ({ items: ['milk'], itemCount: 1 })
