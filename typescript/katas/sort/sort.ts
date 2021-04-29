4a0c6d20623267c389b35951fa08f67a1dbcea00
Tobias Lans <tobias.lans@gmail.com>
2021-04-29 10:06:02 +0200

    New week, new problem: https://open.kattis.com/problems/sort

---
+++ typescript/katas/sort/sort.ts
@@ -0,0 +1,8 @@
+/*
+ * https://open.kattis.com/problems/sort
+ *
+ * The sequence must be sorted so that given any two numbers 𝑋 and 𝑌,
+ * 𝑋 appears before 𝑌 if the number of times 𝑋 appears in the original sequence is larger than the number of time 𝑌 does.
+ * If the number of appearances is equal, the number whose value appears sooner in the input should appear sooner in the sorted sequence.
+ */
+export const frequencySort = (numbers: Array<Number>): Array<Number> => []
