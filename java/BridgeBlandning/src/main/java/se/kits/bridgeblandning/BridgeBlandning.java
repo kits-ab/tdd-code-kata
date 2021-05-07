2b74166549d408edddc9345cb8b24ba825482bbf
Tobias Lans <tobias.lans@gmail.com>
2021-05-07 10:16:05 +0200

    Fix java

--- java/BridgeBlandning/src/main/java/se/kits/bridgeblandning/BridgeBlandning.java
+++ java/BridgeBlandning/src/main/java/se/kits/bridgeblandning/BridgeBlandning.java
@@ -1,25 +1,27 @@
 import java.util.ArrayList;
-import java.util.List;​
+import java.util.List;
 
 public class BridgeBlandning {
-    ​
+
     public static List<String> createDeck() {
-        String[] suits = {"♠️","♥️","♦️","♣️"};
-        String[] values = {"A","K","Q","J","10","9","8","7","6","5","4","3","2"};
-​
+        String[] suits = { "♠️", "♥️", "♦️", "♣️" };
+        String[] values = { "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2" };
+
         List<String> deck = new ArrayList<>();
 
         return deck;
-    }​
+    }
 
     public static List<List<String>> deal() {
         List<String> deck = createDeck();
         List<List<String>> hands = new ArrayList<>();
+
         return hands;
-    }​
+    }
 
     public static List<List<String>> sortHands(List<List<String>> hands) {
         List<String> deck = createDeck();
+
         return hands;
     }
 }
--- java/BridgeBlandning/src/test/java/se/kits/bridgeblandning/BridgeBlandningTest.java
+++ java/BridgeBlandning/src/test/java/se/kits/bridgeblandning/BridgeBlandningTest.java
@@ -1,40 +1,40 @@
-import org.junit.Test;​
-import java.util.List;​
-import static org.junit.Assert.*;​
+import org.junit.Test;
+import java.util.List;
+import static org.junit.Assert.*;
 
 public class BridgeBlandningTest {
-    ​
+
     @Test
     public void createDeck() {
         List<String> result = BridgeBlandning.createDeck();
-​
+
         result.forEach(System.out::println);
         assertEquals(52, result.size());
-    }​
+    }
 
     @Test
     public void deal() {
         List<List<String>> hands = BridgeBlandning.deal();
-​
+
         for (int i = 0; i < 4; i++) {
             hands.get(i).forEach(System.out::print);
             System.out.println();
         }
-​
+
         assertEquals(4, hands.size());
         hands.forEach(a -> assertEquals(13, a.size()));
-    }​
+    }
 
     @Test
     public void sortHands() {
         List<List<String>> hands = BridgeBlandning.sortHands(BridgeBlandning.deal());
-​
+
         for (int i = 0; i < 4; i++) {
             hands.get(i).forEach(System.out::print);
             System.out.println();
         }
-​
+
         assertEquals(4, hands.size());
         hands.forEach(a -> assertEquals(13, a.size()));
-    }​
+    }
 }
